d8ac75e63d58f8a9544259c2c5b73d6144e9e478
gabizinha1551 <142819125+gabizinha1551@users.noreply.github.com>
2025-10-03 12:00:08 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,22 @@
+print("Bora Bil")
+
+print(5+3)
+print(5-2)
+print(5*3)
+print(10/2)
+print(3**20)
+
+idade = 7
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+for i in range(5):
+    print("Número:", i)
+
+
+
+
+
+
